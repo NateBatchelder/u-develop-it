7082cb52e3c66bbf867c52da2347f1094b26bf4f
Nathan Batchelder <natebatchelder@gmail.com>
2022-01-03 20:42:47 -0800

    adding routes for voters

---
+++ routes/apiRoutes/voterRoutes.js
@@ -0,0 +1,17 @@
+router.get("/voters", (req, res) => {
+  const sql = `SELECT * FROM voters`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+
+module.exports = router;
