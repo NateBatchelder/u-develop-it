6c0ab949361e6556e46363bde9042c1ed2bdb8d8
Nathan Batchelder <natebatchelder@gmail.com>
2022-01-04 11:05:19 -0800

    tallies all votes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,13 +3,18 @@ const express = require("express");
 // 
 const router = express.Router();
 
-// 
+// route to get candidates
 router.use(require("./candidateRoutes"));
 
-// 
+// route to get parties
 router.use(require("./partyRoutes"));
 
+// route to get voters
 router.use(require("./voterRoutes"));
+
+// route to caculate votes from voters
+router.use(require("./voteRoutes"));
+
 
 
 
